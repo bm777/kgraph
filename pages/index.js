39d7a071488fb171bb6a0715e8b387ab5983b249
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-04-12 11:50:59 +0100

    Added before args on the GET request

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { getActions, getData } from '../lib/api';
 
 
 
-export default function Home({data}) {
+export default function Home(props) {
   return (
     <div className="container">
       <Head>
@@ -24,8 +24,13 @@ export default function Home({data}) {
 
         {/* Adding the chart */}
         <div className='kgraph'>
+          {
+            props.existing_data ? 
+
+            <KGraph values={props.data}/> : "No data was collected on that period or the given sensor doesn't exist"
+
+          }
           
-          <KGraph values={data}/>
           {/* <KGraph /> */}
             
         </div>
@@ -42,24 +47,42 @@ export default function Home({data}) {
 export async function getServerSideProps(context) {
 
   // get the start time and the name of the device sensor
-  let start = "2022-03-20T09:38:42.500";
-  const _devname = context.query.sensor
+  let d = new Date();
   
+  // let start = "2022-03-10T09:38:42.500Z";
+  
+  const _devname = context.query.sensor
+  const _before = context.query.before
+  d.setDate(d.getDate() - _before);
+  
+  // set the day to today - 'before' days
+  let start = d.toISOString()
+ 
   // get the response data
   const res = await getActions( start )
-  
+
   // parse into json format
   const json = await res.json()
   const _actions = json.actions
+   
 
-  const parsed = getData(_actions, _devname)  
+  const parsed = getData(_actions, _devname)
   // console.log(parsed)
 
   const _data = JSON.stringify(parsed)
+  
+  // check result of _data
+  let existing_data = false
+  if(_data == JSON.stringify({temperature:[],humidity:[],times:[]})){
+    existing_data = false
+  }else{
+    existing_data = true
+  }
 
   return {
     props: {
-      data: _data || JSON.stringify({})
+      data: _data || JSON.stringify({}),
+      existing_data
     }
   }
 }
